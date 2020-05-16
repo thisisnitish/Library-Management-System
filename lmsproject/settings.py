406bd4ae160f2f1daa21817c674dc6b8fa5a395e
Nitish Kumar <nitishkumargenius@gmail.com>
2020-05-17 00:21:16 +0530

    Set Debug Value

--- lmsproject/settings.py
+++ lmsproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['divinelibrary.herokuapp.com']
 
