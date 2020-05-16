f7ae4370722a2ec8d80262ce0834bf57969d5287
Nitish Kumar <nitishkumargenius@gmail.com>
2020-05-16 23:43:38 +0530

    Updated project urls and base.html

--- lmsproject/urls.py
+++ lmsproject/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as user_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +26,4 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html') , name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='lms/home.html') , name='logout'),
     path('', include('lms.urls')),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
